82aaf5fe41a1b43018e4985d54320d9587b46cea
Akshat Goyal <akshatg525@gmail.com>
2026-02-16 13:27:50 +0530

    new organization data will save in DB successfully

--- src/app/api/organizations/route.ts
+++ src/app/api/organizations/route.ts
@@ -1,67 +1,16 @@
-// import { NextResponse } from "next/server";
-// import { createOrganization } from "@/services/organization.service";
-
-// export async function POST(req: Request) {
-//   try {
-//     const { name } = await req.json();
-
-//     if (!name) {
-//       return NextResponse.json(
-//         { error: "Organization name required" },
-//         { status: 400 }
-//       );
-//     }
-
-//     const org = await createOrganization(name);
-
-//     return NextResponse.json(org);
-//   } catch (error: any) {
-//     return NextResponse.json(
-//       { error: error.message || "Something went wrong" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
-// import { createSupabaseServerClient } from "@/lib/supabase/server";
-
-// export async function POST(req: Request) {
-//   const supabase = await createSupabaseServerClient();
-
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser();
-
-//   console.log("USER IN API:", user);
-
-//   return NextResponse.json({ test: true });
-// }
-
-import { NextResponse } from "next/server";
 import { createOrganization } from "@/services/organization.service";
 
-// export async function POST(req: Request) {
 
-//   const { data: test } = await supabase.rpc('auth.uid');
-// console.log("DB AUTH UID:", test);
+export async function POST(req: Request) {
+  try {
+    const { name } = await req.json();
+    const org = await createOrganization(name);
 
-  // try {
-  //   const { name } = await req.json();
-
-    // console.log("Received name:", name);
-
-    // const org = await createOrganization(name);
-
-    // console.log("Created org:", org);
-
-//     return NextResponse.json(org);
-//   } catch (error: any) {
-//     console.error("API ERROR:", error);
-//     return NextResponse.json(
-//       { error: error.message || "Something went wrong" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
+    return Response.json(org, { status: 201 });
+  } catch (error: any) {
+    return Response.json(
+      { error: error.message },
+      { status: 400 }
+    );
+  }
+}
